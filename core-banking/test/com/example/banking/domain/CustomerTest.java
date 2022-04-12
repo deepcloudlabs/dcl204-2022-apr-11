8020487b715716d20bf7a881606a1a9f4c3d5ff0
deepcloudlabs <deepcloudlabs@gmail.com>
2022-04-12 16:22:57 +0300

    Update CustomerTest.java

--- core-banking/test/com/example/banking/domain/CustomerTest.java
+++ core-banking/test/com/example/banking/domain/CustomerTest.java
@@ -17,5 +17,32 @@ class CustomerTest {
 			() -> assertEquals(0, jack.getAccounts().size())			
 		);
 	}
+	
+	@Test
+	void addAccountShouldSuccess() throws Exception {
+		var jack = new Customer("11111111110", "jack bauer");
+		var account1 = new Account("TR1", 100_000);
+		var account2 = new CheckingAccount("TR2", 200_000, 5_000);
+		assertTrue(jack.addAccount(account1));	
+		assertTrue(jack.addAccount(account2));
+		assertFalse(jack.addAccount(account1));
+		assertFalse(jack.addAccount(account2));
+		assertEquals(2, jack.getAccounts().size());
+	}
 
+	
+	@Test
+	void testGetAccountWithIndex() throws Exception {
+		var jack = new Customer("11111111110", "jack bauer");
+		var account1 = new Account("TR1", 100_000);
+		var account2 = new CheckingAccount("TR2", 200_000, 5_000);
+		jack.addAccount(account1);
+		jack.addAccount(account2);
+		assertTrue(jack.getAccount(0).isPresent());
+		assertEquals("TR1",jack.getAccount(0).get().getIban());
+		assertTrue(jack.getAccount(1).isPresent());
+		assertEquals("TR2",jack.getAccount(1).get().getIban());
+		assertTrue(jack.getAccount(2).isEmpty());
+		assertTrue(jack.getAccount(-1).isEmpty());
+	}
 }
